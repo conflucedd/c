4c72c819ac75f2c12ce78341f3ece74455128ee2
conflucedd <confluced@gmail.com>
2022-12-07 23:34:26 +0800

    m

--- fileInclude.c
+++ fileInclude.c
@@ -3,32 +3,87 @@
 #include <string.h>
 #include <stdbool.h>
 
+bool process_complete = false;
+char * process(char *);
+
+int count_str(char *);
+void to_string(FILE *); // return char count
+void to_file(char *);
+
+bool check_include(char *);
+long process_size(char *);
+void put_in(char * in, char * out, fpos_t *);
+
 int main(int str_num, char * str_arg [])
 {
-	FILE * in, * out;
-
-	if(str_num != 1) // read in a
+	FILE * in;
+	FILE * out;
+	
+	char * outfile_name = strcat(str_arg[1], "_with_incl");
+	if(str_num != 1) // read in
 	{
 		exit(1);
 	}
-	if ((in = fopen(str_arg[1], "rb") == NULL))
+	if ((in = fopen(str_arg[1], "r") == NULL))
 	{
 		exit(2);
 	}
+	if ((out = fopen(outfile_name, "wx") == NULL))
+	{
+		exit(3);
+	}
 
-	char * outfile_name = strcat(str_arg[1], "_with_incl");
-	if ((out = fopen(outfile_name, "wxb") == NULL))
+	char * input_str;
+	to_string(in, input_str);
+	to_file(process(input_str));
+
+	if (fclose(in) != 0)
 	{
 		exit(4);
 	}
+	if (fclose(out) != 0)
+	{
+		exit(5);
+	}
 
+	return 0;
+}
 
-	char ch; // start to check
-	fpos_t *include_pos[100]; int incpos_index = 0;
+extern process_complete; // default false
+char * process(char * in_str)
+{
+	char * res_str;
+
+	if (check_include(in_str) == true && process_complete == false)
+	{
+		if ((char * temp = (char *) malloc(process_size(in_str) * sizeof char)) == NULL)
+		{
+			exit(6);
+		}
+		put_in(in_str, temp);
+		free(in_str);
+		process(temp);
+	}
+	else
+	{
+		process_complete = true;
+		res_str = temp;
+		return res_str;
+	}
+}
+
+void to_string(FILE * in)
+{
+	int count = count_str(in);
+	char * 
+}
+
+char ch; // start to check
+	fpos_t include_pos [100]; int incpos_index = 0;
 
 	while ((ch = getc(in)) != EOF)
 	{
-		if (ch == '#' && check_include(in) == true)
+		if (ch != '#' && check_include(in) == true)
 		{
 			fgetpos(in, include_pos[incpos_index]);
 			incpos_index++;
@@ -43,5 +98,12 @@ int main(int str_num, char * str_arg [])
 			put_in(in, getFile(in, include_pos[i]), temp);
 		}
 	}
+
+	if (fclose(in) != 0)
+	{
+		exit(4);
+	}
 	
-}
+
+	return 0;
+
