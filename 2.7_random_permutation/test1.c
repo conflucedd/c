a76540c5abe18ee1e44c6a12c4e1374de74ba089
conflucedd <confluced@gmail.com>
2023-01-14 16:05:17 +0800

    d

--- 2.7_random_permutation/test1.c
+++ 2.7_random_permutation/test1.c
@@ -8,11 +8,11 @@
 bool if_diff(int input, int * array, int input_index);
 int main(void)
 {
-	srand((unsigned)time(NULL));
+	srand((unsigned) time(NULL));
 
 	int a[N];
 
-	int temp_for_assign;
+	int temp_for_assign; // to save the rand value for assign
 	for (int i = 0; i < N; i++)
 	{
 		if (if_diff(temp_for_assign = rand() % N + 1, a, i) == true)
@@ -26,6 +26,8 @@ int main(void)
 		}
 	}
 
+
+// print the answer
 	for (int i = 0; i < N; i++)
 	{
 		printf("%d ", a[i]);
@@ -35,6 +37,8 @@ int main(void)
 
 }
 
+
+// check if the new one is duplicated
 bool if_diff(int input, int * array, int input_index)
 {
 	for (int i = 0; i < input_index; i++)
